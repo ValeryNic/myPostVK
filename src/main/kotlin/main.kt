3608ee32ec5161a0677efd0645f323261a442678
Valery <vn_ulanov@mail.ru>
2023-06-10 17:24:10 +0300

    Autotest update

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,32 +10,36 @@ fun main (){
 }
 data class Post(
     val id: Int=0,//Идентификатор записи.
-    val owner_id: Int=0,//Идентификатор владельца стены, на которой размещена запись. В версиях API ниже 5.7 это поле называется to_id.
-    val from_id: Int=0,//Идентификатор автора записи (от чьего имени опубликована запись).
-    val created_by: Int=0,//Идентификатор администратора, который опубликовал запись (возвращается только для сообществ при запросе с ключом доступа администратора). Возвращается в записях, опубликованных менее 24 часов назад.
+    val ownerId: Int=0,//Идентификатор владельца стены, на которой размещена запись. В версиях API ниже 5.7 это поле называется to_id.
+    val fromId: Int=0,//Идентификатор автора записи (от чьего имени опубликована запись).
+    val createdBy: Int=0,//Идентификатор администратора, который опубликовал запись (возвращается только для сообществ при запросе с ключом доступа администратора). Возвращается в записях, опубликованных менее 24 часов назад.
     val date: Int=1254,//Время публикации записи в формате unixtime.
     val text: String="Good",//Текст записи.
-    val reply_owner_id: Int=0,//Идентификатор владельца записи, в ответ на которую была оставлена текущая.
-    val reply_post_id:Int=0,//Идентификатор записи, в ответ на которую была оставлена текущая.
-    val friends_ounly: Boolean=false,//true, если запись была создана с опцией «Только для друзей».
-    val post_type: String="post",//Тип записи, может принимать следующие значения: post, copy, reply, postpone, suggest.
-    val signer_id: Int=0,//Идентификатор автора, если запись была опубликована от имени сообщества и подписана пользователем;
-    val can_pin: Boolean=false,//Информация о том, может ли текущий пользователь закрепить запись .
-    val can_delete: Boolean=false,//Информация о том, может ли текущий пользователь удалить запись
-    val can_edit: Boolean=false,//Информация о том, может ли текущий пользователь редактировать запись.
-    val is_pinner: Boolean=false,//Информация о том, что запись закреплена
-    val marker_as_ads: Boolean=false,//Информация о том, содержит ли запись отметку «реклама»
-    val is_favorite: Boolean=false,//true, если объект добавлен в закладки у текущего пользователя.
-    val postponed_id: Int=0,//Идентификатор отложенной записи. Это поле возвращается тогда, когда запись стояла на таймере.
-    var postComments: comments = comments(),//Информация о комментариях к записи, объект с полями
-    var postLikes: likes=likes()//Информация о лайках к записи, объект с полями
+    val replyOwnerId: Int=0,//Идентификатор владельца записи, в ответ на которую была оставлена текущая.
+    val replyPostId:Int=0,//Идентификатор записи, в ответ на которую была оставлена текущая.
+    val friendsOunly: Boolean=false,//true, если запись была создана с опцией «Только для друзей».
+    val postType: String="post",//Тип записи, может принимать следующие значения: post, copy, reply, postpone, suggest.
+    val signerId: Int=0,//Идентификатор автора, если запись была опубликована от имени сообщества и подписана пользователем;
+    val canPin: Boolean=false,//Информация о том, может ли текущий пользователь закрепить запись .
+    val canDelete: Boolean=false,//Информация о том, может ли текущий пользователь удалить запись
+    val canEdit: Boolean=false,//Информация о том, может ли текущий пользователь редактировать запись.
+    val isPinner: Boolean=false,//Информация о том, что запись закреплена
+    val markerAsAds: Boolean=false,//Информация о том, содержит ли запись отметку «реклама»
+    val isFavorite: Boolean=false,//true, если объект добавлен в закладки у текущего пользователя.
+    val postPonedId: Int=0,//Идентификатор отложенной записи. Это поле возвращается тогда, когда запись стояла на таймере.
+    var postComments: Comments = Comments(),//Информация о комментариях к записи, объект с полями
+    var postLikes: Likes=Likes()//Информация о лайках к записи, объект с полями
 )
 
 
 object WallService{
-    var posts = emptyArray<Post>()
+    private var posts = emptyArray<Post>()
     var lastId = 0
-    val Error_Limit=-1
+    //val ErrorLimit=-1
+    fun clear(){
+        posts = emptyArray()
+        lastId = 0
+    }
     fun add (post: Post): Post {
         if (lastId>2147483647){
             println("Переполнение счётчика постов")
@@ -46,7 +50,7 @@ object WallService{
     }
     fun likedById(id: Int){
         for ((index, post) in posts.withIndex()){
-            if (post.id==id && post.postLikes.can_likes==true && post.postLikes.user_likes==true && post.postLikes.can_publish==true ){
+            if (post.id==id && post.postLikes.canLikes==true && post.postLikes.userLikes==true && post.postLikes.canPublish==true ){
                 post.postLikes.count = post.postLikes.count++
                 posts[index]=post.copy(postLikes = post.postLikes.copy())
             }
@@ -62,17 +66,17 @@ object WallService{
         return false
     }
 }
-data class comments (
+data class Comments (
     var count: Int=0,
-    var can_post: Boolean=false,
-    var groups_can_post: Boolean=false,
-    var can_close: Boolean=false,
-    var can_open: Boolean=false,
+    var canPost: Boolean=false,
+    var groupsCanPost: Boolean=false,
+    var canClose: Boolean=false,
+    var canOpen: Boolean=false,
     var newComment: String ="New comment"
 )
-data class likes(
+data class Likes(
     var count: Int=0,//число пользователей, которым понравилась запись
-    var user_likes: Boolean=false,//наличие отметки «Мне нравится» от текущего пользователя;
-    var can_likes: Boolean=false,//информация о том, может ли текущий пользователь поставить отметку «Мне нравится» ;
-    var can_publish: Boolean=false//информация о том, может ли текущий пользователь сделать репост записи .
+    var userLikes: Boolean=false,//наличие отметки «Мне нравится» от текущего пользователя;
+    var canLikes: Boolean=false,//информация о том, может ли текущий пользователь поставить отметку «Мне нравится» ;
+    var canPublish: Boolean=false//информация о том, может ли текущий пользователь сделать репост записи .
 )
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -1,46 +1,62 @@
 import org.junit.Assert.*
 import org.junit.Test
+import org.junit.Before
 
 class WallServiceTest {
-
+    @Before
+    fun clearBeforeTest() {
+        WallService.clear()
+    }
     @Test
     fun add1() {
-        var post = Post()
-        var result=false
+        val post = Post(id=0,ownerId=0,fromId=0, createdBy =0,
+            date = 1254, text = "Good", replyOwnerId = 0, replyPostId = 0,
+            friendsOunly = false, postType = "post", signerId = 0,
+            canPin = false,canDelete = false, canEdit = false, isPinner = false,
+            markerAsAds = false,isFavorite = false, postPonedId = 0,
+            postComments = Comments(count = 0,canPost = true,groupsCanPost = true,canClose = true,canOpen = true, newComment ="Good"),
+            postLikes = Likes(count = 0,userLikes = true,canLikes = true,canPublish = true) )
         val newPost: Post = WallService.add(post)
-        if(newPost.id==0) {
-            result = true
-        }
-        assertFalse(result)
+        assertNotEquals(0,newPost.id)
     }
 
     @Test
     fun update1() {
-        val post = Post(id=0,owner_id=0,from_id=0, created_by =0, date = 1254, text = "Good", reply_owner_id = 0, reply_post_id = 0, friends_ounly = false, post_type = "post", signer_id = 0,can_pin = false,can_delete = false, can_edit = false,is_pinner = false,marker_as_ads = false,is_favorite = false, postponed_id = 0, postComments = comments(count = 0,can_post = true,groups_can_post = true,can_close = true,can_open = true, newComment ="Good"), postLikes = likes(count = 0,user_likes = true,can_likes = true,can_publish = true) )
+        val post = Post(id=0,ownerId=0,fromId=0, createdBy =0, date = 1254,
+            text = "Good", replyOwnerId = 0, replyPostId = 0, friendsOunly = false,
+            postType = "post", signerId = 0,canPin = false,canDelete = false,
+            canEdit = false,isPinner = false,markerAsAds = false,isFavorite = false, postPonedId = 0,
+            postComments = Comments(count = 0,canPost = true,groupsCanPost = true,canClose = true,canOpen = true, newComment ="Good"),
+            postLikes = Likes(count = 0,userLikes = true,canLikes = true,canPublish = true) )
+        WallService.add(post)
+        val result=1
         val count:Double= Math.random()
-        var result=1
-        val postN = WallService.add(post)
         val newId: Int = (count * (WallService.lastId-1)).toInt()
         var newPost = WallService.posts[newId]
         newPost.postComments.newComment = "testComment"
-        val note:Boolean = WallService.update(newPost)
-        val post1=WallService.posts[newId]
-        if (post1.postComments.newComment == "testComment") {
-            result = 0
-        }
-        assertEquals(0,result)
+        //val note:Boolean = WallService.update(newPost)
+        //val post1=WallService.posts[newId]
+        //if (post1.postComments.newComment == "testComment") {
+        //    result = 0
+        //}
+        //assertEquals(0,result)
+        assertTrue(WallService.update(newPost))
      }
     @Test
     fun update2() {
-        val post = Post(id=0,owner_id=0,from_id=0, created_by =0, date = 1254, text = "Good", reply_owner_id = 0, reply_post_id = 0, friends_ounly = false, post_type = "post", signer_id = 0,can_pin = false,can_delete = false, can_edit = false,is_pinner = false,marker_as_ads = false,is_favorite = false, postponed_id = 0, postComments = comments(count = 0,can_post = true,groups_can_post = true,can_close = true,can_open = true, newComment ="Good"), postLikes = likes(count = 0,user_likes = true,can_likes = true,can_publish = true) )
+        val post = Post(id=0,ownerId=0,fromId=0, createdBy =0, date = 1254, text = "Good",
+            replyOwnerId = 0, replyPostId = 0, friendsOunly = false, postType = "post",
+            signerId = 0,canPin = false,canDelete = false, canEdit = false,isPinner = false,
+            markerAsAds = false,isFavorite = false, postPonedId = 0,
+            postComments = Comments(count = 0,canPost = true,groupsCanPost = true,canClose = true,canOpen = true, newComment ="Good"),
+            postLikes = Likes(count = 0,userLikes = true,canLikes = true,canPublish = true) )
         val newPost = WallService.add(post)
-        val post1 = Post(id=WallService.lastId,owner_id=0,from_id=0, created_by =0, date = 1254, text = "Good", reply_owner_id = 0, reply_post_id = 0, friends_ounly = false, post_type = "post", signer_id = 0,can_pin = false,can_delete = false, can_edit = false,is_pinner = false,marker_as_ads = false,is_favorite = false, postponed_id = 0, postComments = comments(count = 0,can_post = true,groups_can_post = true,can_close = true,can_open = true, newComment ="Good"), postLikes = likes(count = 0,user_likes = true,can_likes = true,can_publish = true) )
-        val result=WallService.update(post1)
-        var resultInt=if (result==true){
-            1
-        } else {
-            0
-        }
-        assertEquals(0,resultInt)
+        val post1 = Post(id=WallService.lastId,ownerId=0,fromId=0, createdBy =0, date = 1254,
+            text = "Good", replyOwnerId = 0, replyPostId = 0, friendsOunly = false, postType = "post",
+            signerId = 0,canPin = false,canDelete = false, canEdit = false,isPinner = false,
+            markerAsAds = false,isFavorite = false, postPonedId = 0,
+            postComments = Comments(count = 0,canPost = true,groupsCanPost = true,canClose = true,canOpen = true, newComment ="Good"),
+            postLikes = Likes(count = 0,userLikes = true,canLikes = true,canPublish = true) )
+        assertFalse(WallService.update(post1))
     }
 }
