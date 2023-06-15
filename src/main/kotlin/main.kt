9ce872db80bdfe107d57ef97052e9fe696f3a767
Valery <vn_ulanov@mail.ru>
2023-06-15 22:12:34 +0300

    Attachment add

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,29 @@
+import java.awt.Image
+import java.security.CodeSource
 import java.util.Objects
 import javax.xml.stream.events.Comment
+import kotlin.random.Random
 
 fun main (){
     var post = Post(0,0,0,0,1254,"",0,
         0,false,"post",0,false,false,
         false, false, false,false, 0,
-        Comments(),Likes(), Copyright(), Reposts(), arrayOf(0))
+        Comments(),Likes(), Copyright(), Reposts(), views = arrayOf(),
+         copyHistory= arrayOf(0), postDonut = Donut(), attechments = arrayOf()
+    )
     WallService.add(post)
     WallService.add(post)
     WallService.likedById(post.id)
     WallService.update(post)
+    post.id=1
+    post.postComments.newComment="Good update"
+    WallService.update(post)
+    //WallService.update(Post(1, fromId = 1, date=1254, text = "New update"))
+    WallService.print()
+    println(VideoAttachment(Video(1)))
 }
 data class Post(
-    val id: Int=0,//Идентификатор записи.
+    var id: Int=0,//Идентификатор записи.
     val ownerId: Int=0,//Идентификатор владельца стены, на которой размещена запись. В версиях API ниже 5.7 это поле называется to_id.
     val fromId: Int?,//Идентификатор автора записи (от чьего имени опубликована запись).
     val createdBy: Int=0,//Идентификатор администратора, который опубликовал запись (возвращается только для сообществ при запросе с ключом доступа администратора). Возвращается в записях, опубликованных менее 24 часов назад.
@@ -34,8 +45,30 @@ data class Post(
     var postLikes: Likes=Likes(),//Информация о лайках к записи
     var postCopyright: Copyright=Copyright(),//Источник материала
     var postReposts: Reposts= Reposts(),//Информация о репостах записи («Рассказать друзьям»), объект с полями:
-    var views: Array<Int> = arrayOf(0)
+    var views: Array<Int> = arrayOf(0),
+    //val postSource: CodeSource,//Поле возвращается только для Standalone-приложений с ключом доступа, полученным в Implicit Flow.
+    val copyHistory:Array<Int> = arrayOf(0),//<Random.DefaultHistory>, Массив, содержащий историю репостов для записи.
+    val postDonut: Donut=Donut(),
+    val attechments: Array<Attechment> = arrayOf(VideoAttachment(Video(1,1,"My story", "Video", 2048)),  AudioAttachment(Audio(0,0,"Paul McCartny")))
 )
+
+ {
+    override fun equals(other: Any?): Boolean {
+        if(this===other) return true
+        //if (javaClass!=other?.javaClass) return false
+
+        other as Post
+
+        if (id != other.id) return false
+        if (Likes()!= other.postLikes) return false
+        return  attechments.contentEquals(other.attechments)
+    }
+    //override fun hashCode():Int {
+    //    var result = id
+    //    result= 31*result+Likes().hashCode()
+    //    result=31*result+attachments.contentHashCode()
+    //}
+}
 
 
 object WallService{
@@ -50,7 +83,7 @@ object WallService{
         if (lastId>2147483646){
             println("Переполнение счётчика постов")
         } else {
-            posts += post.copy(id= ++lastId, postComments = post.postComments.copy())
+            posts += post.copy(id= ++lastId, postComments = post.postComments.copy(), postLikes = post.postLikes.copy())
         }
         return  posts.last()
     }
@@ -70,6 +103,11 @@ object WallService{
             }
         }
         return false
+    }
+    fun print(){
+        for (post in posts) {
+            print(post)
+        }
     }
 }
 data class Comments (
@@ -92,8 +130,89 @@ data class Copyright(
     var name: String="Name",
     var type: String="Type"
 )
+data class Donut(
+    var isDonut: Boolean=false,//запись доступна только платным подписчика
+    var paidDuration: Int=0,//время, в течение которого запись будет доступна только платным подписчикам VK Donut;
+    //var placeholder: Objects,// заглушка для пользователей, которые не оформили подписку VK Donut. Отображается вместо содержимого записи.
+    var canPublishFreeCopy: Boolean=false,//можно ли открыть запись для всех пользователей, а не только подписчиков VK Donut;
+    var editMode: String= "No typing"//информация о том, какие значения VK Donut можно изменить в записи.
+)
 data class Reposts(//Информация о репостах записи («Рассказать друзьям»), объект с полями:
     var count: Int=0,//число пользователей, скопировавших запись;
     var userReposted: Int=0//наличие репоста от текущего пользователя
 )
 
+//interface  Attechment {
+//    val tupe: String
+//}
+abstract  class Attechment {
+    abstract val type: String
+}
+data class Video(
+    var id: Int=0,//Идентификатор видеозаписи.
+    var ownerId: Int=0,//Идентификатор владельца видеозаписи.
+    var title: String= "My story",//Название видеозаписи
+    var description: String= "Video about my life",//Текст описания видеозаписи
+    var duration: Int=2048//Длительность ролика в секундах
+
+)
+//data class  VideoAttachment(override  val type: String="video", val video:Video ): Attechment
+data class VideoAttachment(val video: Video): Attechment() {
+    override val type: String = "video"
+
+    //get() = TODO("Not yet implemented")
+    override fun toString(): String {
+        return "$type with $video"//super.toString()
+    }
+}
+data class Audio(
+    var id: Int = 0,//Идентификатор аудиозаписи
+    var ownerId:Int = 0,//Идентификатор владельца аудиозаписи
+    var artist: String = "Paul McCarthy",//Исполнитель
+    var title: String = "My love",
+    var duration: Int = 390,
+    var date: Int = 1254
+)
+data class AudioAttachment(val audio: Audio): Attechment(){
+    override  val type: String = "Audio"
+    override fun toString(): String {
+        return "$type with $audio"
+    }
+}
+data class File(
+    var id: Int = 0,//Идентификатор
+    var ownerId:Int = 0,//Идентификатор пользователя, загрузившего файл
+    var title: String = "Help",
+    var size: Int = 4520,
+    var ext: String = ".doc"
+)
+data class FileAttachment( val file: File): Attechment(){
+    override  val type: String = "File"
+    override fun toString(): String {
+        return "$type with $file"
+    }
+}
+data class Sticker(
+    var productId: Int = 0,//Идентификатор набора
+    var stickerId:Int = 0,//Идентификатор стикера
+    var images: Array<Image> = arrayOf(),
+    var size: Int = 4520,
+    var ext: String = ".doc"
+)
+data class StickerAttachment( val sticker: Sticker): Attechment(){
+    override  val type: String = "Sticker"
+    override fun toString(): String {
+        return "$type with $sticker"
+    }
+}
+data class Image(
+    var url: String = "https://netology.ru/profile",
+    var width: Int = 2800,
+    var height: Int = 4200
+)
+data class ImageAttachment( val image: Image): Attechment(){
+    override  val type: String = "Image"
+    override fun toString(): String {
+        return "$type with $image"
+    }
+}
